a46e5365839aa18619d3d52e79ac675c0125d538
YIZIHN <jhlee909090@gmail.com>
2023-07-06 00:21:24 +0900

    [unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+func solution(_ code: String) -> String {
+    var mode = 0
+    var ret = ""
+    
+    for (idx, char) in code.enumerated() {
+        if mode == 0 {
+            if char != "1" {
+                if idx % 2 == 0 {
+                    ret.append(char)
+                }
+            } else {
+                mode = 1
+            }
+        } else {
+            if char != "1" {
+                if idx % 2 != 0 {
+                    ret.append(char)
+                }
+            } else {
+                mode = 0
+            }
+        }
+    }
+    if ret.isEmpty {
+        return "EMPTY"
+    } else {
+        return ret
+        
+    }
+}
+
+
+
+
