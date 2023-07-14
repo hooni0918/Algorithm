77a97233a5234ba9688336a104bafab73f762d57
YIZIHN <jhlee909090@gmail.com>
2023-07-14 10:40:52 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ start:Int, _ end:Int) -> [Int] {
+    
+    var result = [Int]()
+    
+    for num in start...end {
+        result.append(num)
+    }
+    
+    return result
+}
