614bb3c78c4397bcf835e730deab86601f207258
YIZIHN <jhlee909090@gmail.com>
2023-08-09 10:39:10 +0900

    [unrated] Title: 문자열의 앞의 n글자, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ my_string:String, _ n:Int) -> String {
+    let index = my_string.index(my_string.startIndex, offsetBy: n)
+    
+    
+    return String(my_string[..<index])
+}
