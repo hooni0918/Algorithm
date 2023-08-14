45c573d749971ebd73561242d7ecd7471a20fa31
YIZIHN <jhlee909090@gmail.com>
2023-08-14 10:15:23 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ my_string: String, _ is_suffix: String) -> Int {
+    if my_string.hasSuffix(is_suffix) {
+        return 1
+    } else {
+        return 0
+    }
+}
+
