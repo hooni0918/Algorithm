cfc173e6c590d85b2f9ce4196b02e1d3dddaabc4
YIZIHN <jhlee909090@gmail.com>
2023-08-29 00:49:30 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ num_list: [Int]) -> Int {
+    for (index, value) in num_list.enumerated() {
+        if value < 0 {
+            return index
+        }
+    }
+    return -1
+}
