491779fa389d18edb465130c5f1fb5ef4110da6d
YIZIHN <jhlee909090@gmail.com>
2023-11-22 17:08:15 +0900

    [level unrated] Title: 두 수의 합, Time: 0.00 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120802. 두 수의 합/두 수의 합.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    
+    var num1 = num1
+    var num2 = num2
+    
+    var result = num1 + num2
+    
+    
+    
+    return result
+}
