19cb2b910108b4bc4b7b1ea0cc5f5ead313ca686
YIZIHN <jhlee909090@gmail.com>
2023-11-23 18:57:25 +0900

    [level unrated] Title: 배열 두배 만들기, Time: 0.21 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    
+    var results = [Int]()
+    
+    for i in numbers {
+        results.append(i * 2)
+    }
+    
+    
+    return results
+}
