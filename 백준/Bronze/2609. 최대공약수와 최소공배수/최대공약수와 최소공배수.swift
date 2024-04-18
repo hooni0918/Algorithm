c875a561374b86776a8644b81885070dec6bd1d6
YIZIHN <jhlee909090@gmail.com>
2024-04-18 23:20:47 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -0,0 +1,14 @@
+let nums = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+var max = nums.max()!
+var min = nums.min()!
+var r = max%min
+
+while r > 0 {
+    max = min
+    min = r
+    r = max%min
+}
+
+print(min)
+print(nums[0]*nums[1]/min)
