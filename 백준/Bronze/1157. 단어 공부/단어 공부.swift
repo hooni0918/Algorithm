e454660cdbb3f90881a36801563bfcb5f9458137
YIZIHN <jhlee909090@gmail.com>
2024-06-13 15:21:11 +0900

    [Bronze I] Title: 단어 공부, Time: 176 ms, Memory: 81520 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.swift
@@ -0,0 +1,19 @@
+import Foundation
+let word = readLine()!.uppercased()
+var dict: [String:Int] = [:]
+var result: [String] = []
+
+for i in word {
+    if dict[String(i)] == nil {
+        dict[String(i)] = 1
+    }else {
+        dict[String(i)]! += 1
+    }
+}
+for key in dict.keys {
+    if dict[key] == dict.values.max() {
+        result.append(key)
+    }
+}
+
+print(result.count > 1 ? "?" : "\(result[0])" )
