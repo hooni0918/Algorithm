3d43e091ad52c5b31e571220ebbab7f63eecb6fa
YIZIHN <jhlee909090@gmail.com>
2024-12-07 00:57:19 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 1.47 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ arr:[Int], _ delete_list:[Int]) -> [Int] {
+    
+    var result = [Int]()
+    
+    result = arr.filter { !delete_list.contains($0) }
+    
+    return result
+}
