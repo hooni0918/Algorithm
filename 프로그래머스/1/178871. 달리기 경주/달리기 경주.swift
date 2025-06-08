1fe1c9b92d7ff90076779fd2b213efe95a67bd9a
YIZIHN <jhlee909090@gmail.com>
2025-06-08 14:25:56 +0900

    [level 1] Title: 달리기 경주, Time: 1022.11 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.swift
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.swift
@@ -1,30 +1,40 @@
+//10816
+
 import Foundation
 
 func solution(_ players:[String], _ callings:[String]) -> [String] {
- 
-    var playerIndexMap = [String: Int]()
-    var result = players
-
     
-    for (index, player) in result.enumerated() {
-        playerIndexMap[player] = index
+    var horse : [String : Int] = [:]
+    var result = players
+    
+    for (i, player) in result.enumerated() {
+        horse[player] = i
     }
     
-    for i in callings {
-        if let currentIndex = playerIndexMap[i] {
+    for j in callings {
+
+        if let currentIndex = horse[j] {
             
             if currentIndex > 0 {
                 let frontPlayer = result[currentIndex - 1]
                 
-                result[currentIndex - 1] = i
+                result[currentIndex - 1] = j
                 result[currentIndex] = frontPlayer
                 
-                playerIndexMap[i] = currentIndex - 1
-                playerIndexMap[frontPlayer] = currentIndex
-                
+                horse[j] = currentIndex - 1
+                horse[frontPlayer] = currentIndex
             }
+            
+            
         }
-    }
+    
+            
+            
+        }
     
     return result
-}
+    }
+    
+///player 배열을 돌면서 콜링이 불리면
+///플레이어배열내의 순서를 바꿔준다
+///그리고 결과배열에 리턴추가
